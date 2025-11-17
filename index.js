2ec435f776ee6942837ce7370963ef2441acc8ab
Shahfayaz <shahfayaz.softbuilders@gmail.com>
2025-11-17 13:35:30 +0400

    chore: update configs for vercel deployment
    - Add .vercel to gitignore
    - Fix vercel.json build src path
    - Comment out local server listen for serverless deployment

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ app.use((req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 // ❗ IMPORTANT: Export the serverless express handler
 module.exports = app;
